a72ad35b2a8a493b244876967c225a41c9254129
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-09-18 15:26:32 +0200

    Prvá komponenta

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import { Registration } from './components/Registration';
 
 const App = () => {
   return (
     <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>Webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      <img className="user"></img>
+      <Registration
+        username="User Name"
+        email="Email address"
+        password="Password"
+        passwordConfirm="Confirm password"
+      />
+      <button type="submit">REGISTER</button>
     </div>
   );
 };
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
