0d18c5500644f394f3601d9e70b48ff4d5cc69ce
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-09-25 17:09:24 +0200

    Všetky zmeny

---
+++ src/components/Registration/index.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import './style.css';
+import { useState } from 'react';
+
+export const Registration = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const emailChange = (ev) => {
+    setUser({ ...user, email: ev.target.value });
+    if (user.email.indexOf('@') === true && user.username === '') {
+      setUser({
+        ...user,
+        email: ev.target.value,
+        username: ev.target.value.slice(0, user.email.indexOf('@')),
+      });
+    }
+  };
+
+  const usernameChange = () => {
+    setUser({
+      ...user,
+      username: user.email.slice(0, user.email.indexOf('@')),
+    });
+    console.log(user.email.slice(0, user.email.indexOf('@')));
+  };
+
+  const passwordChange = (ev) => {
+    setUser({ ...user, password: ev.target.value });
+  };
+
+  const passwordCheck = (ev) => {
+    setUser({ ...user, passwordConfirm: ev.target.value });
+    //NEFUNGUJE: až po poslednom odkliku a daní medzere vypíše ok, alert reaguje na každý vstup a neviem to odstaviť
+    /* if (user.password === user.passwordConfirm) {
+      alert('Heslo ok.');
+    } else {
+      alert('Hesla se nezhodují');
+    }*/
+  };
+
+  const submit = (ev) => {
+    ev.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <>
+      <form className="column">
+        <input
+          id="column__email"
+          type="text"
+          value={user.email}
+          placeholder="Email Adress"
+          onChange={emailChange}
+        />
+
+        <br />
+
+        <input
+          id="column__name"
+          type="text"
+          placeholder="User Name"
+          value={user.username}
+          onChange={usernameChange}
+        />
+
+        <br />
+
+        <input
+          id="column__password"
+          type="password"
+          placeholder="Password"
+          value={user.password}
+          onChange={passwordChange}
+        />
+
+        <br />
+
+        <input
+          id="column__email"
+          type="password"
+          placeholder="Confirm Password"
+          value={user.passwordConfirm}
+          onChange={passwordCheck}
+        />
+      </form>
+      <button
+        type="submit"
+        disabled={user.password !== user.passwordConfirm} //NEFUNGUJE 🤦‍♀️
+        onClick={submit}
+      >
+        REGISTER
+      </button>
+    </>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -6,14 +6,30 @@ import { Registration } from './components/Registration';
 const App = () => {
   return (
     <div className="container">
-      <img className="user"></img>
-      <Registration
-        username="User Name"
-        email="Email address"
-        password="Password"
-        passwordConfirm="Confirm password"
-      />
-      <button type="submit">REGISTER</button>
+      <h1>REGISTRATION</h1>
+      <div className="logo">
+        <div className="circle"></div>
+        <svg
+          className="user"
+          xmlns="http://www.w3.org/2000/svg"
+          width="70"
+          height="70"
+          version="1"
+          viewBox="0 0 512 512"
+        >
+          <path
+            d="M2335 5105c-273-42-517-172-708-375-210-223-319-481-334-790-21-432 191-845 557-1084 180-118 353-178 563-195 389-30 721 92 989 365 213 216 321 452 347 753 36 402-116 776-419 1038-196 168-376 252-625 288-120 17-260 18-370 0z"
+            transform="matrix(.1 0 0 -.1 0 512)"
+          ></path>
+          <path
+            d="M1427 2639c-452-48-766-372-912-944-97-381-122-839-60-1090 51-201 218-405 411-503 87-44 212-80 317-92 62-8 534-10 1437-8l1345 3 85 24c316 86 532 298 611 596 41 155 36 525-12 817-105 650-371 1040-792 1162-96 28-268 49-319 39-49-9-117-45-271-144-164-106-187-118-310-164-142-53-259-76-396-76-140 0-248 20-390 70-134 48-140 51-350 184-110 70-192 115-224 124-56 14-56 14-170 2z"
+            transform="matrix(.1 0 0 -.1 0 512)"
+          ></path>
+        </svg>
+      </div>
+      <div className="container_box">
+        <Registration />
+      </div>
     </div>
   );
 };
